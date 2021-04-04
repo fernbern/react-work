0162fd0f048ef61981224e598e661c299a520043
fernbern <70728008+fernbern@users.noreply.github.com>
2021-04-03 22:03:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import MaterialUI from 'material-ur;
 
 import { store, persistor } from './redux/store';
 
