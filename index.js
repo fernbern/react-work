b7705fc23dc1f3b346de421f8cda826df0f6dbb0
fernbern <70728008+fernbern@users.noreply.github.com>
2021-04-25 23:34:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux/persist/integration/react';
 import { store, persistor } from '../redux/store';
 import './index.css';
+import 'style.css';
+
 import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate>
-        <App/>
+        <App prop={window}/>
       </PersistGate>
     </BrowserRouter>
   </Provider>,
